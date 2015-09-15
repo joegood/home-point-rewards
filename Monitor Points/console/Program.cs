d10eec63940a2cf2b47aa50fd4ba069689552cf0
Joe Good <joe.d.good@gmail.com>
2015-09-14 22:33:19 -0500

    Added: Initial skeleton console apps for all four app types. Changed:  Added Ninject NuGet package.

---
+++ Monitor Points/console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home_points_monitor_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
